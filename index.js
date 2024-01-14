e1494eb08bc5880a80ad454725b13945be681526
kylestech95 <kylestech95@outlook.com>
2024-01-13 20:55:02 -0500

    feat: get request

--- index.js
+++ index.js
@@ -18,7 +18,49 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+//test for unix time with a regex test
+function ifUnix(unixTime){
+  return /^\d+$/.test(unixTime)
+}
+// convert valid date to unix time (milliseconds)
+function convert2UTC(unix){
+  return +unix+((60000*60)*5)
+}
+// format utc string
+function formatUTC(day,dayNum,month,year,time,zone,utc){
+      return `${day}, ${dayNum} ${month} ${year} ${time} ${zone}`
+}
+// request to unix & request to utc
+app.get("/api/:date?", (req,res)=>{
+  let { date } = req.params
+  let unix, utc;
+  if(ifUnix(date)){
+    unix = date;
+    let temp_utc = new Date(convert2UTC(unix)).toString(); 
+    let arr = temp_utc.split` `
+    let day = arr[0]
+    let month = arr[1]
+    let dayNum = arr[2]
+    let year = arr[3]
+    let time = arr[4]
+    let zone = arr[5].slice(0,3)
+    utc = formatUTC(day,dayNum,month,year,time,zone,utc)
+      }
+  else{
+    unix = new Date(date).getTime();
+    let temp_utc = new Date(convert2UTC(unix)).toString(); 
+    let arr = temp_utc.split` `
+    let day = arr[0]
+    let month = arr[1]
+    let dayNum = arr[2]
+    let year = arr[3]
+    let time = arr[4]
+    let zone = arr[5].slice(0,3)
+    utc = formatUTC(day,dayNum,month,year,time,zone,utc)
+  }
+      res.json({unix:unix,utc:utc})
+      
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
